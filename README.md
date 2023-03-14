166f84301d157199ad291ba250d5cc4d87d81a6d
Madalyn Elwood <maddy.elwood@icloud.com>
2023-03-14 10:37:27 -0500

    README.md install.sh and remote-install.sh

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # dotfiles
+
+## Installation
+
+To install, type these commands:
+
+'''
+git clone https://github.com/maddyelwood/dotfiles .dotfiles
+cd .dotfiles && chmod u+x install.sh && ./install.sh link
+'''
+
---
+++ install.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+link() {
+    rm -f ~/.vimrc ~/.inputrc
+    ln -sf ~/.dotfiles/.vimrc ~/.vimrc
+    ln -sf ~/.dotfiles/.inputrc ~/.inputrc
+}
+
+unlink() {
+    rm -f ~/.vimrc ~/.inputrc
+    cp ~/.dotfiles/.vimrc ~/.vimrc
+    cp ~/.dotfiles/.inputrc ~/.inputrc
+}
+
+# ./install.sh link
+# ./install.sh unlink
+
+if [[ $# -ne 1]] ; then
+    echo "Usage: $0 (link|unlink)"
+    exit 1
+fi
+
+# Actually link or unlink here...
+
+if [[ $1 != "link" && $1 != "unlink" ]] ; then
+    echo "Invalid subcommand $1."
+    echo "Usage: $0 (link|unlink)"
+    exit 1
+fi
+
+# Do it! (link or unlink)
+
+$1
